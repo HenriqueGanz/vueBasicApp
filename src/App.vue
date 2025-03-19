e4f872c090ba56793e5397a8971bb1c58719d86c
Henrique Ganz <henrique.ganz@gmail.com>
2025-03-19 04:56:40 -0300

    mudanca no titulo do app

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,11 @@ import { RouterView } from 'vue-router'
   <div class="h-screen flex flex-col bg-gray-900 gap-2">
     <header class="bg-gray-800 w-full min-h-25 flex justify-center items-center">
       <nav>
-        <span class="font-bold text-xl sm:text-2xl text-white">
-          Welcome to a basic app made with Vue 3!
+        <h1 class="font-bold text-xl sm:text-2xl text-white">
+          Welcome to a basic app!
+        </h1>
+        <span class="text-sm text-green-500">
+          Made with Vue 3!
         </span>
       </nav>
     </header>
